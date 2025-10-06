c56e6c292c19022045d855bc5429fc21b7fcb121
Krishna Dobhal <krishnadobhal234@gmail.com>
2025-10-06 23:52:31 +0530

    Refactor Experience and Footer components for improved layout and responsiveness; adjust SVG dimensions for better visual consistency.

--- src/Component/Footer/page.tsx
+++ src/Component/Footer/page.tsx
@@ -4,28 +4,25 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <div className="bg-white p-4 max-w-full">
-            <div className="grid grid-cols-12">
-                <div className="col-span-1"></div>
-                <div className="col-span-8 flex text-2xl items-center text-black font-extrabold gap-3">
-                    <Svg />
-                    <div className="">Krishna Dobhal</div>
-                </div>
-                <div className="col-span-3">
-                    <div className="flex gap-7 text-4xl text-right">
-                        <div className="">
-                            <Link href={"https://github.com/krishnadobhal/"}>
-                                <FaGithub />
-                            </Link>
-                        </div>
-                        <Link href={"https://www.linkedin.com/in/krishnadobhal/"}>
-                            <FaLinkedin />
+        <div className="bg-white p-4 max-w-full flex justify-between items-center" >
+            <div className="flex text-xl md:text-2xl items-center text-black font-extrabold gap-3">
+                <Svg />
+                <div className="">Krishna Dobhal</div>
+            </div>
+            <div className="">
+                <div className="flex gap-7 text-2xl md:text-4xl text-right">
+                    <div className="">
+                        <Link href={"https://github.com/krishnadobhal/"}>
+                            <FaGithub />
                         </Link>
-                        <div className="">
-                            <Link href={"https://discord.com/users/885423663380332594"}>
-                                <FaDiscord />
-                            </Link>
-                        </div>
+                    </div>
+                    <Link href={"https://www.linkedin.com/in/krishnadobhal/"}>
+                        <FaLinkedin />
+                    </Link>
+                    <div className="">
+                        <Link href={"https://discord.com/users/885423663380332594"}>
+                            <FaDiscord />
+                        </Link>
                     </div>
                 </div>
             </div>
