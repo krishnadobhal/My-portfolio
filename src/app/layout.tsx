af4441bfccaac6a33c562de8bdb9fcbbba2c0616
Krishna Dobhal <krishnadobhal234@gmail.com>
2026-05-12 13:06:22 +0530

    Refactor layout to include external script
    Added a Script component for external script loading.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
-"use client"
-import { Sora } from 'next/font/google'
+"use client";
+
+import { Sora } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
-
 const sora = Sora({
-  subsets: ['latin'],
-  display: 'swap',
-})
- 
-
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -17,9 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={sora.className}
-      >
+      <head>
+        <Script
+          src="https://pub-64d7c9742ee54006ae9f38e02aa8539e.r2.dev/a579f40d-6c51-4292-92d0-89a72fd8a12b/script.min.js"
+          data-pid="a579f40d-6c51-4292-92d0-89a72fd8a12b"
+          strategy="afterInteractive"
+        />
+      </head>
+
+      <body className={sora.className}>
         {children}
       </body>
     </html>
